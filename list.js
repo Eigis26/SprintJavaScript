5c167edbdafaa3c14cfe774afa8000f7857945b8
Gintarė Ūsaitė <eigirdas.user@gmail.com>
2022-04-26 16:12:20 +0300

    Adding upadate function to shopping list and styled button

--- list.js
+++ list.js
@@ -9,10 +9,16 @@ function addElement() {
 function display(){
     document.getElementById("list").innerHTML = "";
     for (i = 0; i < elements.length; i++)
-    document.getElementById("list").innerHTML += "<div class='element'>"+elements[i]+"<input type='button' id='delete' value='delete' onclick='del("+i+")'></div>";
+    document.getElementById("list").innerHTML += "<div class='element'>"+elements[i]+"<input type='button' id='delete' value='delete' onclick='del("+i+")'><input type='button' id='update' value='update' onclick='upd("+i+")'></div>";
 } 
 function del(index){
     elements.splice(index, 1);
     display();
 } 
- 
+function upd(index){
+    updateText = prompt("Let's update it!");
+    if (updateText != ""){
+        elements[index] = updateText;
+    } 
+    display();
+} 
